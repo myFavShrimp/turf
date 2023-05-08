cfb9fe1638cfe1c462ef80b395cba6d954669f27
Denis <42263661+myFavShrimp@users.noreply.github.com>
2023-05-08 08:04:29 +0000

    Switched macros and lib

--- turf/src/lib.rs
+++ turf/src/lib.rs
@@ -1,20 +1,14 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, LitStr};
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
-#[proc_macro]
-pub fn style_sheet(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as LitStr);
-    let path_str = input.value();
-    let css = grass::from_path(path_str, &grass::Options::default()).unwrap();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let style = stylist::Style::new(css).unwrap();
-    let class_name = style.get_class_name();
-    let style_sheet = style.get_style_str();
-
-    let output = quote! {
-        static CLASS_NAME: &'static str = #class_name;
-        static STYLE_SHEET: &'static str = #style_sheet;
-    };
-    output.into()
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
--- turf_macros/src/lib.rs
+++ turf_macros/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, LitStr};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro]
+pub fn style_sheet(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as LitStr);
+    let path_str = input.value();
+    let css = grass::from_path(path_str, &grass::Options::default()).unwrap();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let style = stylist::Style::new(css).unwrap();
+    let class_name = style.get_class_name();
+    let style_sheet = style.get_style_str();
+
+    let output = quote! {
+        static CLASS_NAME: &'static str = #class_name;
+        static STYLE_SHEET: &'static str = #style_sheet;
+    };
+    output.into()
 }
