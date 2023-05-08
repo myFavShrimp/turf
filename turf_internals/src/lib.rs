bf68ec75ca6f889ecbbc09a3ef5be4490262e805
Denis <42263661+myFavShrimp@users.noreply.github.com>
2023-05-08 08:30:07 +0000

    Added Deserialize for Settings

--- turf_internals/src/lib.rs
+++ turf_internals/src/lib.rs
@@ -1,10 +1,14 @@
 use std::path::PathBuf;
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
 pub struct Settings {
     output_style: OutputStyle,
     load_paths: Vec<PathBuf>,
 }
 
+#[derive(Deserialize)]
 pub enum OutputStyle {
     Expanded,
     Compressed,
