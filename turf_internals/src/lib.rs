ade0a92c8b53d3eb1174517387a9b0378d988262
Denis <42263661+myFavShrimp@users.noreply.github.com>
2023-05-08 08:36:22 +0000

    Added conversion from Settings to grass::Options

--- turf_internals/src/lib.rs
+++ turf_internals/src/lib.rs
@@ -8,8 +8,25 @@ pub struct Settings {
     load_paths: Vec<PathBuf>,
 }
 
+impl<'a> Into<grass::Options<'a>> for Settings {
+    fn into(self) -> grass::Options<'a> {
+        grass::Options::default()
+            .style(self.output_style.into())
+            .load_paths(&self.load_paths)
+    }
+}
+
 #[derive(Deserialize)]
 pub enum OutputStyle {
     Expanded,
     Compressed,
+}
+
+impl Into<grass::OutputStyle> for OutputStyle {
+    fn into(self) -> grass::OutputStyle {
+        match self {
+            OutputStyle::Expanded => grass::OutputStyle::Expanded,
+            OutputStyle::Compressed => grass::OutputStyle::Compressed,
+        }
+    }
 }
