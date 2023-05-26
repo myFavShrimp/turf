19eef1e938c4dd7e65399d19bc2a51d0f74cc36b
myFavShrimp <myfavshrimp@tuta.io>
2023-05-27 00:07:49 +0200

    Added class name randomization base

--- lib/turf_internals/src/transformer.rs
+++ lib/turf_internals/src/transformer.rs
@@ -4,10 +4,23 @@ use lightningcss::{
     visit_types,
     visitor::{Visit, VisitTypes, Visitor},
 };
-use std::convert::Infallible;
+use std::{collections::HashMap, convert::Infallible};
 
-struct MyVisitor;
-impl<'i> Visitor<'i> for MyVisitor {
+#[derive(Default)]
+struct TransformationVisitor {
+    classes: HashMap<String, String>,
+}
+
+impl TransformationVisitor {
+    fn randomized_class_name(&mut self, class_name: String) -> String {
+        match self.classes.get(&class_name) {
+            Some(random_class_name) => random_class_name.clone(),
+            None => String::new(),
+        }
+    }
+}
+
+impl<'i> Visitor<'i> for TransformationVisitor {
     type Error = Infallible;
 
     const TYPES: VisitTypes = visit_types!(SELECTORS);
@@ -16,12 +29,9 @@ impl<'i> Visitor<'i> for MyVisitor {
         dbg!(&selectors);
 
         for selector in selectors.iter_mut_raw_match_order() {
-            match selector {
-                Component::Class(c) => {
-                    dbg!(&c);
-                    *c = format!("{}", c).into();
-                }
-                _ => {}
+            if let Component::Class(c) = selector {
+                dbg!(&c);
+                *c = format!("{}", c).into();
             }
         }
 
@@ -36,7 +46,7 @@ pub fn transform_stylesheet(
     let mut stylesheet = StyleSheet::parse(css, ParserOptions::default())?;
 
     stylesheet
-        .visit(&mut MyVisitor)
+        .visit(&mut TransformationVisitor::default())
         .expect("css visitor never fails");
 
     let css_result = stylesheet.to_css(settings.into()).unwrap();
