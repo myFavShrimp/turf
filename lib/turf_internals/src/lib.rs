b5005a49982d2035f380db8f20a35e5afc76078e
myFavShrimp <myfavshrimp@tuta.io>
2023-06-19 12:25:02 +0200

    Improved error message by providing a file path

--- lib/turf_internals/src/lib.rs
+++ lib/turf_internals/src/lib.rs
@@ -5,6 +5,8 @@ mod manifest;
 mod settings;
 mod transformer;
 
+use std::path::{Path, PathBuf};
+
 pub use settings::Settings;
 
 #[derive(thiserror::Error, Debug)]
@@ -12,13 +14,29 @@ pub enum Error {
     #[error("error parsing cargo manifest - {0}")]
     TomlParseError(#[from] toml::de::Error),
     #[error("error reading cargo manifest - {0}")]
-    IoError(#[from] std::io::Error),
-    #[error("error compiling scss - {0}")]
-    GrassError(#[from] Box<grass::Error>),
+    ManifestError(#[from] std::io::Error),
+    #[error("error reading path '{1}' - {0}")]
+    PathError(std::io::Error, PathBuf),
+    #[error("error compiling scss file '{1}' - {0}")]
+    GrassError(Box<grass::Error>, PathBuf),
     #[error("error transforming css - {0}")]
     CssError(#[from] transformer::LightningcssError),
     #[error("error obtaining random id - {0}")]
     RandError(#[from] getrandom::Error),
+}
+
+impl<P> From<(Box<grass::Error>, P)> for Error
+where
+    P: AsRef<Path> + std::fmt::Debug,
+{
+    fn from(value: (Box<grass::Error>, P)) -> Self {
+        let canonicalized_path = value.1.as_ref().canonicalize();
+
+        match canonicalized_path {
+            Ok(path) => Error::GrassError(value.0, path),
+            Err(e) => Error::PathError(e, value.1.as_ref().to_path_buf()),
+        }
+    }
 }
 
 fn random_seed() -> Result<u64, getrandom::Error> {
--- lib/turf_internals/src/macro_functions.rs
+++ lib/turf_internals/src/macro_functions.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, path::Path};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
 fn style_sheet_with_compile_options<P>(
     path: P,
@@ -7,7 +10,12 @@ fn style_sheet_with_compile_options<P>(
 where
     P: AsRef<Path> + std::fmt::Debug,
 {
-    let css = grass::from_path(path, &settings.clone().into())?;
+    let css = grass::from_path(&path, &settings.clone().into()).map_err(|e| {
+        let mut manifest_path = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+        manifest_path.push(path);
+
+        crate::Error::from((e, manifest_path))
+    })?;
     crate::transformer::transform_stylesheet(&css, settings)
 }
 
