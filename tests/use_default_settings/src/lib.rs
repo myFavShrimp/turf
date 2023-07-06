833e6420b1bcc955c29d9f4bdcc13e39dad38f7f
myFavShrimp <myfavshrimp@tuta.io>
2023-07-06 18:13:45 +0200

    Adjusted tests for new stylesheet path logic

--- tests/use_default_settings/src/lib.rs
+++ tests/use_default_settings/src/lib.rs
@@ -1,6 +1,6 @@
 #[test]
 fn use_default_settings() {
-    turf::style_sheet!("use_default_settings/src/mystyle.scss");
+    turf::style_sheet!("src/mystyle.scss");
     assert!(STYLE_SHEET.ends_with("{color:#333}"));
     assert!(STYLE_SHEET.starts_with(".class-"));
     assert!(STYLE_SHEET.starts_with(&format!(".{}", ClassName::TEST)));
