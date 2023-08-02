33379b5eb3b0229527a96e7ab1b8013c03ccc0f2
myFavShrimp <myfavshrimp@tuta.io>
2023-08-02 17:56:52 +0200

    Made path canonicalization assume that paths are relative to the Cargo manifest dir

--- lib/turf_internals/src/path.rs
+++ lib/turf_internals/src/path.rs
@@ -4,7 +4,9 @@ pub fn canonicalize<P>(path: P) -> PathBuf
 where
     P: AsRef<Path>,
 {
-    let mut manifest_path = PathBuf::from(std::env::current_dir().unwrap());
+    let mut manifest_path = PathBuf::from(
+        std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR environment variable"),
+    );
     manifest_path.push(path);
 
     manifest_path
